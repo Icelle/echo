80d0b5a0dde59d410d157009bee565d815ccf401
Icelle <icelleku@gmail.com>
2013-11-24 12:36:36 -0500

    phase2

--- echo.rb
+++ echo.rb
@@ -1,5 +1,11 @@
 def playback(say)
-  return "You said: #{say}"
+  if say == "Nothing!"
+    return "Ok, fine!"
+  elsif say == "I have a lot to say"
+    return "I don't have time for that right now!"
+  else
+    return "You said: #{say}"
+  end
 end
 
 puts "What do you want to say?"
